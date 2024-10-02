16d7f962c76ec8dfa70adecdc301b91eb1cbd19e
mad-dnav <mad-dnav@Mac-mini-de-admin.local>
2024-10-02 11:59:49 +0200

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*****************************************/
+/* Ignoramos esta parte por el momento */
+/*****************************************/
+let intervalId = window.setInterval(function () {
+    console.log("Clock isn't working yet!");
+}, 10000);
+
+function stopClock() { 
+    clearInterval(intervalId);
+}
+/*****************************************/
+
+window.onload = function () { 
+    console.log("Hello, world!");
+}
