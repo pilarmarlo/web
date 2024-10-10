5a8aed940e9ee7ca313419e9fadc918db27cd48e
Pilar <pilar.martinez03@mail.ucv.es>
2024-10-10 14:36:09 +0200

    Clase día 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 /* Ignoramos esta parte por el momento */
 /*****************************************/
 let intervalId = window.setInterval(function () {
-    console.log("Clock isn't working yet!");
+    //console.log("Clock isn't working yet!");
 }, 10000);
 
 function stopClock() { 
@@ -11,5 +11,22 @@ function stopClock() {
 /*****************************************/
 
 window.onload = function () { 
-    console.log("Hello, world!");
+   const miReloj = document.getElementById('clock');
+   console.log(miReloj);
+   console.dir(miReloj);
+   miReloj.innerText= "hola, hola";
+
+   const headers = document.getElementsByTagName('div');
+   console.dir(headers);
+}
+
+function randInt(min,max){
+    return Math.floor(Math.random()*(max - min + 1)+ min)
+}
+
+function changeEmojiColor(elem){
+    const r = randInt(0,225);
+    const g = randInt(0,255);
+    const b = randInt(0,255);
+    elem.style.fill = 'rgb(' + r + ',' + g + ',' + b + ')';
 }
